0ee51e276b8985f20505d91fda70d353285b1551
QuyNyHoang <74020738+QuyNyHoang@users.noreply.github.com>
2025-05-18 17:24:31 +0200

    Setup Maven project and add GitHub Packages repo + prose-builder dependency

---
+++ src/main/java/com/quynyhoang/App.java
@@ -0,0 +1,13 @@
+package com.quynyhoang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
